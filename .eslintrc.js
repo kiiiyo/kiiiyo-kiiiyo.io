9dfa9293050f43cbf53a1929249a368125765bea
kiiiyo <kiiiyo.jp@gmail.com>
2021-03-07 12:37:18 +0900

    :package: add linter pakages

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  settings: { react: { version: 'detect' } },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended', // TypeScript rules
+    'plugin:react/recommended', // React rules
+    'plugin:react-hooks/recommended', // React hooks rules
+    'plugin:@typescript-eslint/recommended', // TypeScript rules
+    'prettier/@typescript-eslint', // Prettier plugin
+    'plugin:prettier/recommended', // Prettier recommended rules
+    'plugin:tailwind/recommended', // Tailwind plugin
+  ],
+  rules: {
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'jsx-a11y/anchor-is-valid': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    // Includes .prettierrc.js rules
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+  },
+}
